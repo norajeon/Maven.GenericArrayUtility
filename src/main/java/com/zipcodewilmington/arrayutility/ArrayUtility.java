63d80ec45ca56a82ee30966f25d03159c518f333
nora <sxj042@gmail.com>
2023-03-31 21:15:53 -0400

    finished

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,6 +1,10 @@
 package com.zipcodewilmington.arrayutility;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by leon on 3/6/18.
@@ -46,10 +50,58 @@ public class ArrayUtility<T> {
                 mergedArray.remove(mergedArray.get(i));
             }
         }
-        return (T[]) mergedArray.toArray();
+//        for(int j = 0; j < mergedArray.size(); j++) {
+//
+//        }
+//new instance creates new array
+        T[] items = (T[]) Array.newInstance(mergedArray.get(0).getClass(), mergedArray.size());
+
+        for (int j = 0; j <mergedArray.size(); j++) {
+            items[j] = mergedArray.get(j);
+        }
+
+
+        return items;
+
+//        return (T[]) Arrays.copyOf(mergedArray);
+
+//        return (T[]) mergedArray.toArray();
     }
 
     public T getMostCommonFromMerge(T[] arrayToMerge) {
-        return null;
+
+        for(T ans : arrayToMerge) {
+            mergedArray.add(ans);
+        }
+        int count = 1, tempC;
+        T frequent = mergedArray.get(0);
+        T temp;
+
+        for (int i = 0; i < mergedArray.size()-1; i++) {
+            temp = mergedArray.get(i);
+            tempC = 0;
+            for (int j = 1; j < mergedArray.size(); j++) {
+                if (temp == mergedArray.get(j)) {
+                    tempC++;
+                }
+            }
+            if (tempC > count) {
+                frequent = temp;
+                count = tempC;
+            }
+
+        }    return frequent;
+
+//    Map<T, Integer> map = new HashMap<>();
+//        for (T a : arrayToMerge) {
+//            if (map.containsKey(a)) {
+//                map.put(a, map.get(a) + 1);
+//            }
+//            else {
+//                map.put(a, 1);
+//            }
+//        }
+
+
     }
 }
